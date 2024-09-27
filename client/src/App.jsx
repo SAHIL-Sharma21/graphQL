453efb06d1172f5947e1b61590b4946825d441be
Sahil <sahilsharma2000dtc@gmail.com>
2024-09-27 14:32:03 +0530

    implemented graphql in client in react application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {gql, useQuery} from '@apollo/client'
+
+const query = gql`
+  query GetTodosWithUser{
+    getTodos {
+      id
+      title
+      completed
+      user {
+        id
+        name
+      }
+    }
+  }
+`
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const {data, loading, error} = useQuery(query);
+
+  if (loading){
+    <h1>Loading...</h1>
+  }
+
+  if(error){
+    return <h1>Error:{error.message}</h1>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>  
+      <main>
+        <div>
+          <table>
+            <tbody>
+              {data?.getTodos?.map((todo) => <tr key={todo.id}>
+                <td>{todo.title}</td>
+                <td>{todo.user?.name}</td>
+              </tr>)}
+            </tbody>
+          </table>
+        </div>
+      </main>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+  cache: new InMemoryCache(),
+});
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
